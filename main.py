1065cc92c25608396b99eb422512662034792c48
Lukasbank <lukas.banklu@gmail.com>
2025-04-20 20:18:24 +0200

    finished the project

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
-from stats import get_book_text, book_text_string
+from stats import word_count, get_book_text, book_text_string, each_character, sorted_char_list, sort_on, print_list
+import sys
 
+def main():
+    book_text = book_text_string()
+    character_dict = each_character(book_text)
+    sort_list = sorted_char_list(character_dict)
+    file_path = sys.argv[1]
+    
+    print(f"\
+============ BOOKBOT ============]\n\
+Analyzing book found at {file_path}...\n\
+---------- Word Count ----------\n\
+Found {word_count(book_text)} total words\n\
+--------- Character Count -------")
+    print_list(sort_list)
+    print("============= END ===============")
+    
+    
 
-def main(book_text_string):
-    word_list = book_text_string.split()
-    num_words = len(word_list)
-    print(f"{num_words} words found in the document")
-
-        
-
-
-main(book_text_string())
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:    
+    main()
 
 
 
--- stats.py
+++ stats.py
@@ -1,25 +1,43 @@
+import sys
+
+def sort_on(dict):
+    return dict["count"]
+
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
         return file_contents
     
 def book_text_string():
-    book_text = get_book_text("books/frankenstein.txt")
+    book_text = get_book_text(sys.argv[1])
     return(book_text)
+
+def word_count(book_text_string):
+    word_list = book_text_string.split()
+    num_words = len(word_list)
+
+    return num_words
 
 def each_character(book_text_string):
     each_character_dict = {}
     lower_case_text = book_text_string.lower()
     for character in lower_case_text:
-        if character in lower_case_text:
+        if character in each_character_dict:
             each_character_dict[character] += 1
         else:
             each_character_dict[character] = 1
 
+    return each_character_dict
 
+def sorted_char_list(character_dict):
+    character_list = []
+    for character, count in character_dict.items():
+        if character.isalpha():
+            character_list.append({"character": character, "count": count})
+    character_list.sort(reverse=True,key=sort_on)
+    return character_list
 
-    print(each_character_dict)
-
-each_character(book_text_string())
-
+def print_list(sort_list):
+    for pair in sort_list:
+        print(str(pair["character"]) + ": " + str(pair["count"]))
 
